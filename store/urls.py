f1d71da1b8577428500e278e819e71088f784ccf
Aashir  Waqar <aashir.waqar@A006-00550.local>
2023-07-26 16:13:14 +0500

    Changed from API view to Generic Views

--- store/urls.py
+++ store/urls.py
@@ -4,10 +4,10 @@ from . import views
 # URLConf
 urlpatterns = [
     path("products/", views.ProductList.as_view()),
-    path("products/<int:id>/", views.ProductDetail.as_view()),
+    path("products/<int:pk>/", views.ProductDetail.as_view()),
     path("collections/", views.CollectionList.as_view()),
     path(
-        "collections/<int:id>/",
+        "collections/<int:pk>/",
         views.CollectionDetail.as_view(),
         name="collection-detail",
     ),
