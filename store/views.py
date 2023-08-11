6a4bb3d5193249d6e24c7c34fcf6a54061e750d5
Aashir  Waqar <aashir.waqar@A006-00550.local>
2023-08-11 21:19:00 +0500

    Code for Shopping cart api

--- store/views.py
+++ store/views.py
@@ -5,11 +5,24 @@ from rest_framework.decorators import api_view
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.viewsets import ModelViewSet
-from .models import Collection, Product, OrderItem, Review
+from rest_framework.mixins import (
+    CreateModelMixin,
+    RetrieveModelMixin,
+    DestroyModelMixin,
+)
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from .models import Collection, Product, OrderItem, Review, Cart, CartItem
 from .filters import ProductFilter
 from .pagination import DefaultPagination
-from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
+from .serializers import (
+    CollectionSerializer,
+    ProductSerializer,
+    ReviewSerializer,
+    CartSerializer,
+    CartItemSerializer,
+    AddCartItemSerializer,
+    UpdateCartItemSerializer,
+)
 
 
 class ProductViewSet(ModelViewSet):
@@ -56,3 +69,29 @@ class ReviewViewSet(ModelViewSet):
 
     def get_serializer_context(self):
         return {"product_id": self.kwargs["product_pk"]}
+
+
+class CartViewSet(
+    CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet
+):
+    queryset = Cart.objects.prefetch_related("items__product").all()
+    serializer_class = CartSerializer
+
+
+class CartItemViewSet(ModelViewSet):
+    http_method_names = ["get", "post", "patch", "delete"]
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return AddCartItemSerializer
+        elif self.request.method == "PATCH":
+            return UpdateCartItemSerializer
+        return CartItemSerializer
+
+    def get_serializer_context(self):
+        return {"cart_id": self.kwargs["cart_pk"]}
+
+    def get_queryset(self):
+        return CartItem.objects.filter(cart_id=self.kwargs["cart_pk"]).select_related(
+            "product"
+        )
