91eb7a8b69a744109ee97309c831de6c3042dfee
Aashir  Waqar <aashir.waqar@A006-00550.local>
2023-08-18 20:12:32 +0500

    Created Orders api

--- store/models.py
+++ store/models.py
@@ -93,11 +93,11 @@ class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
     class Meta:
-        permissions = [("cancel_order",'Can Cancel order')]
+        permissions = [("cancel_order", "Can Cancel order")]
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name="items")
     product = models.ForeignKey(
         Product, on_delete=models.PROTECT, related_name="orderitems"
     )
